bdc70139e5060b83e3c29669fb07fa84d10dd46b
iiyahia <yahiapro400@gmail.com>
2025-11-25 20:38:34 +0200

    Switch from GitHub Pages to Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/yahia-store',
-    assetPrefix: '/yahia-store/',
     images: {
         unoptimized: true,
         remotePatterns: [
